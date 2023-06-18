ca84c9e975b07284cc05d9a51d5af456298d1002
Funakurabe <k020g1164@g.neec.ac.jp>
2023-06-19 05:21:18 +0900

    Tlasに組み込む用のモデルデータを用意。

---
+++ KazLibrary/Raytracing/BlasVector.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <DirectXMath.h>
+#include <vector>
+#include <memory>
+#include <wrl.h>
+#include <d3d12.h>
+
+namespace Raytracing
+{
+
+	class Blas;
+
+	//BottomLevelAccelerationStructureに必要なデータをまとめたクラス
+	//レイトレで描画するオブジェクトのBLASを毎フレームこれに登録する。
+	class BlasVector {
+
+	private:
+
+		/*===== メンバ変数 =====*/
+
+		std::vector<std::weak_ptr<Blas>> m_refBlas;	//レイトレで描画するBlasの参照 (毎フレーム登録する。)
+		std::vector<D3D12_RAYTRACING_INSTANCE_DESC> m_instanceDesc;	//BlasをTlasに登録するために変形した状態。
+
+
+	public:
+
+		/*===== メンバ関数 =====*/
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		BlasVector();
+
+		/// <summary>
+		/// 更新処理 主に配列を空にする。
+		/// </summary>
+		void Update();
+
+		/// <summary>
+		/// レイトレとして描画するために配列に追加。
+		/// </summary>
+		/// <param name="arg_refBlas"></param>
+		/// <param name="arg_worldMat"></param>
+		void Add(std::weak_ptr<Blas> arg_refBlas, const DirectX::XMMATRIX& arg_worldMat);
+
+		/// <summary>
+		/// 保存されている参照の数。
+		/// </summary>
+		int GetBlasRefCount();
+
+		/// <summary> 
+		/// 参照元のシェーダーレコードにアクセスして書き込む。
+		/// </summary>
+		/// <param name="arg_dest"> 書き込み先ポインタ </param>
+		/// <param name="arg_recordSize"> 書き込むサイズ </param>
+		/// <param name="arg_stateObject"> 書き込むオブジェクト </param>
+		/// <param name="arg_hitGroup"> 使用するHitGroupの名前 </param>
+		/// <returns> 書き込み後のポインタ </returns>
+		uint8_t* WriteShaderRecord(uint8_t* arg_dest, UINT arg_recordSize, Microsoft::WRL::ComPtr<ID3D12StateObject>& arg_stateObject, LPCWSTR arg_hitGroup);
+
+	};
+
+}
