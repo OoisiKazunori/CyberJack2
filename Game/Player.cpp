733bb8d762ee24c33ee262d68f717a39ae99c93b
OoishiKazunori <cinc.higahi@gmail.com>
2023-08-24 04:20:46 +0900

    プレイヤーのUI削除

--- Game/Player.cpp
+++ Game/Player.cpp
@@ -280,10 +280,10 @@ void Player::Draw(DrawingByRasterize& arg_rasterize, Raytracing::BlasVector& arg
 
 	*/
 
-	if (drawHpFlag)
-	{
-		hpUi.Draw(arg_rasterize);
-	}
+	//if (drawHpFlag)
+	//{
+	//	hpUi.Draw(arg_rasterize);
+	//}
 
 	m_transform.scale = { 50.0f,50.0f,50.0f };
 	//m_transform.pos = { 0.0f,10.0f,50.0f };
